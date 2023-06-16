98f7e8847ae03fdf9ec669c828b282aeac95f206
Jeff Rohalla <jeff@braidit.com>
2023-06-16 13:37:02 -0500

    Setup script example

---
+++ src/scripts/example.sh
@@ -0,0 +1,14 @@
+#  This file contains example bash scripts to accomplish various FFMPEG tasks
+
+# watermark in bottom left corner
+# ffmpeg ...
+
+# watermark in bottom right corner
+
+# watermark in top left corner
+
+# watermark in top right corner
+
+# attach preroll to beginning of video
+
+# attach endroll to end of video
