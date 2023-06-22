1623120abec601304722aef6329f45fda9e3af46
ez55 <85459013+ez55@users.noreply.github.com>
2023-06-22 16:34:45 -0500

    Initial work (#1)
    * Setup script example
    
    * Add roadmap to readme
    
    * bash attempt
    
    * Update bash script
    
    * temp
    
    * temp
    
    * added ffmpeg examples to example.sh
    
    * added folder with samples of the seven ffmpeg commands in examples.sh
    
    * fixed typo in file name
    
    * Removed output files and added *.mp4 to .gitignore
    
    ---------
    
    Co-authored-by: Jeff Rohalla <jeff@braidit.com>

---
+++ src/scripts/example.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+input_1=$1
+input_2=$2
+
+echo "Input 1: $input_1"
+echo "Input 2: $input_2"
+#  This file contains example bash scripts to accomplish various FFMPEG tasks
+
+# watermark in bottom left corner
+#ffmpeg -i sample.mp4 -i logo.png -filter_complex '[1]colorchannelmixer=aa=0.5[logo2], [0][logo2]overlay=15:H-h-15' output.mp4
+
+# watermark in bottom right corner
+#ffmpeg -i sample.mp4 -i logo.png -filter_complex '[1]colorchannelmixer=aa=0.5[logo2], [0][logo2]overlay=W-w-15:H-h-15' output.mp4
+
+# watermark in top left corner
+#ffmpeg -i sample.mp4 -i logo.png -filter_complex '[1]colorchannelmixer=aa=0.5[logo2], [0][logo2]overlay=15:15' output.mp4
+
+# watermark in top right corner
+#ffmpeg -i sample.mp4 -i logo.png -filter_complex '[1]colorchannelmixer=aa=0.5[logo2], [0][logo2]overlay=W-w-15:15' output.mp4
+
+# watermark in center
+#ffmpeg -i sample.mp4 -i logo.png -filter_complex '[1]colorchannelmixer=aa=0.5[logo2], [0][logo2]overlay=(W-w)/2:(H-h)/2' output.mp4
+
+# attach preroll to beginning of video
+# intro must have an audio stream
+#ffmpeg -i intro.mp4 -i sample.mp4  -filter_complex "[0:v][0:a][1:v][1:a]concat=n=2:v=1:a=1[v][a]" -map "[v]" -map "[a]" output.mp4
+
+# attach endroll to end of video
+# outro must have an audio stream
+# ffmpeg -i sample.mp4 -i outro.mp4 -filter_complex "[0:v][0:a][1:v][1:a]concat=n=2:v=1:a=1[v][a]" -map "[v]" -map "[a]" output.mp4
+
+file_name="text"
+echo "new file beings:" > $input_1
+touch $input_1
+echo $file_name
+for i in {1..10}
+do
+  echo "Welcome $i times\n" >> $input_1
+done
